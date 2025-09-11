fcef9db37d04a1cbfe4fd7b6b9e9ac95fae69095
grug-mehatecomputer <ray.hummel@mehatecomputer.com>
2025-09-10 22:40:06 -0400

    Add files via upload

---
+++ 0_template.ps1
@@ -0,0 +1,18 @@
+﻿# Composed by Grug
+# WARNING - NOT FOR THOSE WITHOUT COMPREHENSION
+
+#SCRIPT PURPOSE :
+
+#LOGGING
+$SCRIPTNAME ="Something"
+$DATETIME = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$LOGPATH = "C:\TEMP\$SCRIPTNAME_$DATETIME.txt"
+Start-Transcript -path $LOGPATH -append
+
+#SCRIPT VARS
+
+
+#
+
+
+Stop-Transcript
