1b8422c5f4543f6e16d258f6293e25f7d1437aaf
grug-mehatecomputer <ray.hummel@mehatecomputer.com>
2026-02-03 10:46:37 -0500

    Add files via upload

---
+++ 2_sharePoint_exportOneDriveURLs.ps1
@@ -0,0 +1,27 @@
+﻿﻿# Composed by Grug
+# WARNING - NOT FOR THOSE WITHOUT COMPREHENSION
+
+#SCRIPT PURPOSE :
+#Exports all -my.sharepoint.com/personal/ URLS for review. Probably for use with the SharePoint Migration Tool
+
+#LOGGING
+$SCRIPTNAME ="exportOneDriveURL"
+$DATETIME = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$LOGPATH = "C:\TEMP\$SCRIPTNAME_$DATETIME.txt"
+Start-Transcript -path $LOGPATH -append
+
+#SCRIPT VARS
+
+$TenantUrl = Read-Host "Enter the SharePoint admin center URL"
+
+Connect-SPOService -Url $TenantUrl
+
+$Export = "C:\TEMP\OneDriveURLs.log"
+
+
+Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url | Out-File $Export -Force
+
+Write-Host "Done! File saved as $($Export)."
+
+
+Stop-Transcript
